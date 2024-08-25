b410aa35855c17c34da8f28193aaa89a6b25de07
Bulty-T <shraddhaforpython@proton.me>
2024-08-25 09:02:01 +0000

    jsss

---
+++ script.js
@@ -0,0 +1,77 @@
+// this print to browser console
+//console.log("hi"+"hello");
+// con-cat-e-nation //
+
+//console.log(prompt("enter name"));
+//console.log("hi","hello");
+//console.log(2+ prompt());
+
+//let jar = prompt("enter juice name");
+//console.log(jar)
+
+//let fruit = "kiwi"
+//fruit = "orange"
+//console.log(fruit)
+
+//let fruit
+//console.log(fruit)
+
+// not a number // NAN
+//let userInput = Number(prompt("enter a number"))
+//console.log(userInput + 23)
+
+//let userInput = (prompt('enter a number'))
+//console.log(userInput+23)
+
+//const fruit = "kiwi"
+//console.log(fruit)
+
+//const fruit = "kiwi"
+//fruit = "orange"
+//console.log(fruit)
+
+//const fruit = null;
+//console.log(10.*.fruit); // implicit-type conversion
+// both show nothingness void
+// undefined =automatic (let fruit; = undefined)(implicit)
+// null = manual (const fruit = null;)(explicit)
+// undefined = not a number , null as 0
+
+// declare
+//function sum(){
+//    console.log("hi hello");
+//}
+//sum()
+//sum()
+
+//let a = 2; // gobal scope becz outside
+//function sum(){
+    //let b = 3;
+  //  console.log(a+b)
+//}
+//console.log(a)
+//sum()
+//sum()
+
+//function sum(num1,num2){
+  //  console.log(num1 + num2);
+//}
+//sum(1, 2)
+
+//function sum(num1, num2){
+  //  return num1 + num2;
+//}
+//let a = sum(5, 2)
+//console.log(a)
+
+//function sum(num1 ,num2){
+  //  return num1 * num2;
+//}
+//let a = sum(5, 3)
+//console.log(a)
+
+//function double(num){
+  //  return num * 2;
+//}
+//let a = double(78)
+//console.log(a)
